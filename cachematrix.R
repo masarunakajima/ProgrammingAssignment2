f6e12cc5ba69561c549c4bd2dd7cfbb0d6bc3e9a
masarunakajima <masarun@usc.edu>
2022-06-04 00:27:25 -0700

    Defined makeCacheMatrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+      x <<- y
+      m <<- NULL
+  }
+  get <-function() x
+  setinv <- function(inv) m<<- inv
+  getinv <- function() inv
+  list(set = set, get = get, 
+       setinv = setinv, getinv = getinv)
 }
 
 
